9248cbc34e520b7bdb1056a82fd1057419c76837
jatinpanchal1o1 <jatin1o1panchal@gmail.com>
2023-10-16 16:40:47 +0530

    new-files

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+<!-- <img src="/preview.gif" alt="preview" width="600px" /> -->
 
-Currently, two official plugins are available:
+## Follow Below instructions to add your deatils in the portfolio.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### Change and customize every section according to your need, All you need to do is edit `/src/Details.js`
+
+#### Open `/src/Details.js` & modify it as per your need.
+
+```javascript
+/ Enter your Personal Details here
+export const personalDetails = {
+  name: "Jatin Panchal",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Dec 2021",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = { ... };
+
+// Tech Stack and Tools
+export const techStackDetails = { ... };
+
+// Enter your Project Details here
+export const projectDetails = [ ... ];
+
+// Enter your Contact Details here
+export const contactDetails = { ... };
+```
+>>>>>>> a9c66f8 (new-file)
---
+++ src/Details.js
@@ -0,0 +1,147 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.png";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Jatin Panchal",
+  tagline: "I build things for web",
+  img: profile,
+  about: `Throughout my career, I have demonstrated strong leadership abilities by effectively leading cross-functional teams and collaborating with stakeholders to deliver high-quality IT solutions. I thrive in fast-paced environments, adapt quickly to emerging technologies, and remain committed to staying up-to-date with the latest industry trends and certifications.
+
+  If you are seeking a dedicated and dynamic IT professional who can drive organizational success through innovative technology solutions, I would be delighted to connect and explore potential opportunities.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/jatinpanchal1o1",
+  github: "https://github.com/jatinpanchal1o1",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Junior Software Developer",
+    Company: `Collectent`,
+    Location: "Gurugaon",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Apr 2022",
+  },
+  {
+    Position: "Associate Analyest",
+    Company: `Global Logic`,
+    Location: "Gurugaon",
+    Type: "Full Time",
+    Duration: "Sep 2022 - Aug 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Science in Information Technology",
+    Company: "Amity University",
+    Location: "Gurugaon",
+    Type: "Full Time",
+    Duration: "Jun 2018 - Jun 2021",
+  },
+  {
+    Position: "Computer Science 50",
+    Company: `Harvard`,
+    Location: "Massachusetts",
+    Type: "Online",
+    Duration: "Aug 2020 - Sep 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Login Page",
+    image: projectImage1,
+    description: `Simple Login page with use of html,css,js,bootstrap,etc.`,
+    techstack: "HTML,CSS,JavaScript,Bootstrap",
+    previewLink: "https://jatinpanchal1o1.github.io/web-project/login-page.html",
+    githubLink: "https://github.com/jatinpanchal1o1/web-project",
+  },
+  {
+    title: "Website on Sass",
+    image: projectImage2,
+    description: `Ecommers website based on Sass css, which shop products online.`,
+    techstack: "HTML,CSS,JavaScript,Bootstrap,Sass",
+    previewLink: "https://jatinpanchal1o1.github.io/SASS-Site/",
+    githubLink: "https://github.com/jatinpanchal1o1/SASS-Site",
+  },
+  {
+    title: "Sun Dail",
+    image: projectImage3,
+    description: `Try to achive sun dail system by using react, and there precomponent`,
+    techstack: "Node.js,React.js,Tailwind,Chart.js,etc.",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "React Project",
+    image: projectImage4,
+    description: `Using React component to create react dashboard for retrieving details`,
+    techstack: "Node.js,React.js,Mui,etc.",
+    previewLink: "https://jatinpanchal1o1.github.io/inter-project/",
+    githubLink: "https://github.com/jatinpanchal1o1/inter-project",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "Jatin121panchal@gmail.com",
+  phone: "+91 92576-30048",
+};
